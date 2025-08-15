5497fd803f20464da4d32a64e3631fe48ec34c4f
Anbt0106 <buithoan_t67@hus.edu.vn>
2025-08-15 11:45:21 +0700

    load products

--- app/routes/views.py
+++ app/routes/views.py
@@ -1,15 +1,27 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from ..models.product import Product
+from sqlalchemy.sql import func
+import random
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template('home.html', user=current_user)
+    # Lấy 5 sản phẩm random từ database
+    products = Product.query.order_by(func.random()).limit(8).all()
+    return render_template('home.html', products=products)
+
+@views.route('/products')
+def products():
+    # Lấy tất cả sản phẩm từ database
+    all_products = Product.query.all()
+    return render_template('products.html', products=all_products)
 
 @views.route('/new-arrivals')
 def new_arrivals():
     return render_template('new-arrivals.html', user=current_user)
+
 
 @views.route('/account')
 @login_required
